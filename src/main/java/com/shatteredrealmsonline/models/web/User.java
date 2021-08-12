b04700b6a6e3642ad01a3ebc8f670ca3ced87b42
Wil Simpson <simpson.wil255@gmail.com>
2021-08-12 01:18:04 -0500

    Updated character list to fetch eager.

--- src/main/java/com/shatteredrealmsonline/models/web/User.java
+++ src/main/java/com/shatteredrealmsonline/models/web/User.java
@@ -60,7 +60,7 @@ public class User implements Serializable
 
     @Getter
     @Setter
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_characters")
     private List<Character> characters;
 
